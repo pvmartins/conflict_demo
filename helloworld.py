533cdec4afcd1e74334498e5074dbeaf7620a211
PAULO MARTINS <73678853+pvmartins@users.noreply.github.com>
2023-01-27 21:45:09 -0500

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
 
 def main():
     hello_world('NYC', 'New York')
-
-
-main()
-=======
-def hello_world(city):
-    print(f'Hello World {city}!')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> 53301173b07028d490d707a889a626c96fb44d64
 
 
 main()
